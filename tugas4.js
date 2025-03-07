592eb3b4d94b2bb1a5b78903c6c64056435a784c
nadiradkvlya <nadira.kaivalya@cakrawala.ac.id>
2025-03-08 02:27:31 +0700

    Update tugas4.js

--- tugas4.js
+++ tugas4.js
@@ -1,5 +1,5 @@
 let startDate = new Date ("2024-03-19")
-let endDate = new Date ("2024-03-25")
-let betweenDate = endDate.getTime() - startDate.getTime()
+let endtDate = new Date ("2024-03-25")
+let betweenDate = endtDate.getTime() - startDate.getTime()
 betweenDate = betweenDate / (1000 * 3600 * 24)
 console.log (betweenDate)
